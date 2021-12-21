8d865728138132e6cfd5b93039502259350f3f01
Swathi.G <60893570+0715Swathi@users.noreply.github.com>
2021-12-21 17:37:00 +0530

    Create movies.sql

---
+++ movies.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Movies (
+    Movie_Name      VARCHAR (30) PRIMARY KEY,
+    Actor           CHAR (30)    NOT NULL,
+    Actress         CHAR (30)    NOT NULL,
+    Director        CHAR (30)    NOT NULL,
+    Year_Of_release INTEGER (30) NOT NULL
+);
